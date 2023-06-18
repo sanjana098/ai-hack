e5f4c33e7e03a025f248e91d865a70da06fd87f9
adrianlam15 <93681064+adrianlam15@users.noreply.github.com>
2023-06-17 20:40:43 -0700

    Configured api endpoints for receiving data

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,15 @@
 # API endpoints for the backend
 # imports
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS
 from langchain.llms import OpenAI
 
 app = Flask(__name__)
 CORS(app)
 
-OPENAI_API_KEY = ""
+OPENAI_API_KEY = "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"
 llm = OpenAI(openai_api_key=OPENAI_API_KEY)
+MODEL = "gpt-4-0613"
 
 
 # main route
@@ -18,10 +19,11 @@ def main():
 
 
 # generate response route
-@app.route("/api/response")
+@app.route("/api/response", methods=["POST"])
 def generateResponse():
-    response = llm.predict("")
-    return {"response": response}
+    data = request.get_json()
+    response = OpenAI.generate(llm, MODEL, data)
+    return {"message": data, "response": response}
 
 
 # get clinical trials route
