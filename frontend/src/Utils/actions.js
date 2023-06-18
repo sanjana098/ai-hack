d19cba72998afa650c3d3b37a20ca89a156ab3a8
Mohan Raj <mohanraj746007@gmail.com>
2023-06-18 02:50:55 -0700

    ui action changes

---
+++ frontend/src/Utils/actions.js
@@ -0,0 +1,40 @@
+const API_URL = "http://127.0.0.1:5000";
+
+export const mainRoute = () => {
+  return fetch(`${API_URL}/`)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+};
+
+export const generateResponse = (data) => {
+  return fetch(`${API_URL}/api/response`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+};
+
+export const getClinicalTrials = () => {
+  return fetch(`${API_URL}/api/clinicalTrials`)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+};
