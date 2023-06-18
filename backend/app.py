b132b10c75bf52a362207a37d8b823876a2d2751
adrianlam15 <93681064+adrianlam15@users.noreply.github.com>
2023-06-17 19:56:46 -0700

    Added api endpoints

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,35 @@
+# API endpoints for the backend
+# imports
 from flask import Flask
 from flask_cors import CORS
+from langchain.llms import OpenAI
 
 app = Flask(__name__)
 CORS(app)
 
+OPENAI_API_KEY = ""
+llm = OpenAI(openai_api_key=OPENAI_API_KEY)
 
+
+# main route
 @app.route("/")
-def Main():
-    return {"success": True}
+def main():
+    return {"success": True, "route": "main"}
 
 
+# generate response route
+@app.route("/api/response")
+def generateResponse():
+    response = llm.predict("")
+    return {"response": response}
+
+
+# get clinical trials route
+@app.route("/api/clinicalTrials")
+def getClinicalTrials():
+    return {"success": True, "route": "clinicalTrials"}
+
+
+# main
 if __name__ == "__main__":
     app.run(debug=True)
